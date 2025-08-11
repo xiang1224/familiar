3ca4ba70784cccd0d7cf021f003c60adf7bd3a79
xiang1224 <a10083237@gmail.com>
2025-08-11 21:37:09 +0800

    0811-9

--- js/script.js
+++ js/script.js
@@ -127,7 +127,7 @@ const usersDB = [
     {
         callsign: "977",
         username: "977",
-        password: "977",
+        password: "1234",
         role: "資深急救員",
         name: "李逸庭",
         level: "1",
